78dc5684e8770d87c6d1806084e1199722993fca
aninda123456 <anindachakrabarty4@gmail.com>
2019-01-18 09:56:48 +0530

    Added feature to send Images and Captions

--- telegramEmailBot.py
+++ telegramEmailBot.py
@@ -2,6 +2,7 @@ import requests
 import json
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 import time
 import re
@@ -13,8 +14,6 @@ def addUser(uid,first_name,last_name=None):
     userSettings[uid]["fName"]=first_name
     if(last_name!=None):
         userSettings[uid]["lName"]=last_name
-    #userSettings[uid]["email"]=email
-    #userSettings[uid]["password"]=password
 def addEmail(uid,email):
     userSettings[uid]["email"]=email
 def addPassword(uid,password):
@@ -41,13 +40,14 @@ def sendErrorMsg(msg,uid):
     text = "%20".join(msg.split(" "))
     url = URL+AUTH+"/sendMessage?text="+text+"&chat_id="+str(uid)
     requests.get(url)
-def sendEmail(uid,msg):
-    sender="abcd@def.com" #Put the sender email here
-    password="xxxxxxxxxxx" #Put the sender passoword here
+def sendEmail(uid,msg,imageContent):
+    sender="hackerboui47@gmail.com"
+    password="opm161.cm"
     sub="Telegram Message"
     portNo = "587"
     if("email" not in userSettings[uid]):
         sendErrorMsg("Can't send message since email is not set",uid)
+        print(userSettings)
     else:
         reciever=userSettings[uid]["email"]
         msgFormatted = MIMEMultipart()
@@ -55,6 +55,9 @@ def sendEmail(uid,msg):
         msgFormatted["To"]=reciever
         msgFormatted["Subject"]=sub
         msgFormatted.attach(MIMEText(msg,"plain"))
+        if(imageContent!=None):
+            msgFormatted.attach(MIMEImage(imageContent))#todo
+            print("im")
         server = smtplib.SMTP('smtp.gmail.com',int(portNo))
         server.ehlo()
         server.starttls()
@@ -71,7 +74,14 @@ def retName(uid):
         return (name + " " + userSettings[uid]["lName"])
     else:
         return name
+def extractFilePath(file_id):
+    url = URL+AUTH+"/getFile?file_id="+file_id
+    filePathJson = requests.get(url)
+    filePathDict = json.loads(filePathJson.text)
+    return "https://api.telegram.org/file/bot"+AUTH+"/"+filePathDict["result"]["file_path"]
 def onePassScan(offset=None):
+    msg=""
+    imageContent=None
     jsonDict,offset=extractJSON(offset)
     print("1")
     if(len(jsonDict["result"])==0):
@@ -82,13 +92,23 @@ def onePassScan(offset=None):
         else:
             addUser(jsonDict["result"][0]["message"]["from"]["id"],jsonDict["result"][0]["message"]["from"]["first_name"])
     else:
+        print("ping")
         emailREGX = re.compile(r"/Email[\s]*-[\s]*([\w\._+-]+@[\w\._+-]+.com)")
-        if(emailREGX.search(jsonDict["result"][0]["message"]["text"])!=None):
-            email = emailREGX.search(jsonDict["result"][0]["message"]["text"]).group(1)
-            addEmail(jsonDict["result"][0]["message"]["from"]["id"],email)     
-        else:
-            msg = jsonDict["result"][0]["message"]["text"]
-            sendEmail(jsonDict["result"][0]["message"]["from"]["id"],msg+"\nMessage was sent by: "+retName(jsonDict["result"][0]["message"]["from"]["id"]))
+        if("text" in jsonDict["result"][0]["message"]):
+            if(emailREGX.search(jsonDict["result"][0]["message"]["text"])!=None):
+                email = emailREGX.search(jsonDict["result"][0]["message"]["text"]).group(1)
+                addEmail(jsonDict["result"][0]["message"]["from"]["id"],email)
+                return offset
+            else:
+                msg = jsonDict["result"][0]["message"]["text"]
+        if("photo" in jsonDict["result"][0]["message"]):
+            file_id = jsonDict["result"][0]["message"]["photo"][-1]["file_id"]
+            filePath = extractFilePath(file_id)
+            if("caption" in jsonDict["result"][0]["message"]):
+                msg=msg+jsonDict["result"][0]["message"]["caption"]
+            print(filePath)
+            imageContent = requests.get(filePath,stream=True).content
+        sendEmail(jsonDict["result"][0]["message"]["from"]["id"],msg+"\nMessage was sent by: "+retName(jsonDict["result"][0]["message"]["from"]["id"]),imageContent)
     return offset
 def runScrypt():
     offset = None
